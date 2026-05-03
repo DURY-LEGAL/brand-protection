cc19d5dc07156324f280ef5dc15d549a1792cd95
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:41:56 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// brand-protection - Brand protection toolkit - Monitor and protect your brand online. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
